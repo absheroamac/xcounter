04a582b65ccf54996f0e5b4caa1228e3667e349a
Absher K <absheroamac@gmail.com>
2024-10-19 12:26:19 +0530

    Converted to class Based

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import { useState } from "react";
+import { Component } from "react";
 
-function App() {
+class App extends Component {
 
-  const [count,setCount] = useState(0);
+  constructor(props){
+    super(props)
+
+    this.state = {
+      count:0
+    }
+
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  decrement = ()=>{
+    this.setState(prev=>({
+      count:prev.count-1
+    }))
+  }
+
+
+ render(){
 
 
 
   return (
     <div className="App">
       <h1>Counter App</h1>
-      <div>Count: {count}</div>
-      <button onClick={()=>setCount(prev=>prev+1)}>Increment</button>
-      <button onClick={()=>setCount(prev=>prev-1)}>Decrement</button>
+      <div>Count: {this.state.count}</div>
+      <button onClick={this.increment}>Increment</button>
+      <button onClick={this.decrement}>Decrement</button>
     </div>
   );
+}
 }
 
 export default App;
